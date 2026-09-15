62192005f113da4a3a00a8cdb0e5b4b1ef7cb3f0
SWIFTSTACK <swiftstack@proton.me>
2026-09-15 17:14:45 -0600

    Feat: Modulo de Inventario, Proveedores y Guias de Envio (v1.6.0-PROD) - app/modules/guias/index.php

---
+++ app/modules/guias/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * TCS MOTRIZ - Módulo de Guías de Envío (Alias)
+ * Redirección transparente al módulo de logística y despachos
+ */
+
+if (!defined('TCS_ACCESS')) {
+    exit('Acceso denegado');
+}
+
+require_once __DIR__ . '/../envios/index.php';
