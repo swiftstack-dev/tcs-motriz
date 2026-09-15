1288b6d349a2e3b417d2af710a91150f8d6fb586
SWIFTSTACK <swiftstack@proton.me>
2026-09-15 15:15:57 -0600

    Feat: Add executive KPI reporting panel and nav enhancements (app/index.php)

--- app/index.php
+++ app/index.php
@@ -398,7 +398,7 @@ $equipos = DataStore::getEquipos();
             <a href="index.php?view=reportes" class="nav-item <?= ($view === 'reportes' ? 'active' : '') ?>">
                 <div class="nav-label-group">
                     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/></svg>
-                    <span>Reportes Mantenimiento</span>
+                    <span>Reportes e Informes</span>
                 </div>
             </a>
 
